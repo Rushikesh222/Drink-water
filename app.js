7ce749507902b4537bddc913521c59f79396f3c0
Rushikesh Shirsat <shirsatrushikesh6@gmail.com>
2021-11-21 12:46:37 +0530

    complete

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const Smallcups = document.querySelectorAll(".cup-small");
 const listers = document.getElementById("liters");
 const percentage = document.getElementById("percentage");
 const remained = document.getElementById("remained");
+upadtebigcups();
 
 Smallcups.forEach((cup, idx) => {
   cup.addEventListener("click", () => highlightCups(idx));
@@ -21,4 +22,24 @@ function highlightCups(idx) {
       cup.classList.remove("full");
     }
   });
+  upadtebigcups();
+}
+function upadtebigcups() {
+  const fullcups = document.querySelectorAll(".cup-small.full").length;
+  const totalcups = Smallcups.length;
+  if (fullcups === 0) {
+    percentage.style.visibility = "hidden";
+    percentage.style.height = 0;
+  } else {
+    percentage.style.visibility = "visible";
+    percentage.style.height = `${(fullcups / totalcups) * 330}px`;
+    percentage.innerText = `${(fullcups / totalcups) * 100}%`;
+  }
+  if (fullcups === totalcups) {
+    remained.style.visibility = "hidden";
+    remained.style.height = 0;
+  } else {
+    remained.style.visibility = "visible";
+    listers.innerText = `${2 - (250 * fullcups) / 1000}L`;
+  }
 }
